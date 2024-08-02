a195f1145ad7b84bb64600c3684a473028962a9d
Gevorg <gulkanyangk@mail.ru>
2024-08-02 13:12:33 +0500

    Fixed AplInstaller

--- installapplication/src/main/java/com/xbet/installapplication/ApkInstaller.java
+++ installapplication/src/main/java/com/xbet/installapplication/ApkInstaller.java
@@ -1,9 +1,11 @@
 package com.xbet.installapplication;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 
 import java.io.File;
 import java.util.Objects;
@@ -22,10 +24,10 @@ public class ApkInstaller {
 
                 uri = InstallApplicationFileProvider.getUriForFile(Objects.requireNonNull(context),
                         "com.xbet.installapplication.installapplicationfileprovider", file);
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION;
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_SINGLE_TOP;
             } else {
                 uri = Uri.fromFile(file);
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK;
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP;
             }
 
             intent.setDataAndType(uri, "application/vnd.android.package-archive");
@@ -39,6 +41,36 @@ public class ApkInstaller {
             }
         } else {
             callback.onInstallFailed("File does not exist");
+        }
+    }
+
+    private static boolean CanPackageInstallAllowed(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return context.getPackageManager().canRequestPackageInstalls();
+        } else {
+            // На версиях Android ниже O это разрешение управляется вручную пользователем
+            return true;
+        }
+    }
+
+    public static void OpenSettings(Context context) {
+        Intent intent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
+            intent.setData(Uri.parse("package:" + context.getPackageName()));
+        } else {
+            // На версиях Android ниже O перенаправляем пользователя в основные настройки
+            intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
+        }
+        context.startActivity(intent);
+    }
+
+    public static void RestartApp(Context context) {
+        if (context instanceof Activity) {
+            Activity activity = (Activity) context;
+            Intent intent = activity.getIntent();
+            activity.finish();
+            activity.startActivity(intent);
         }
     }
 
